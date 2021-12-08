73c55abccacc8688a0acdbc449b75da97d487ce7
uhyeon19 <uhyeon1009@gmail.com>
2021-12-08 11:17:46 +0900

    buzzer

--- project.py
+++ project.py
@@ -6,13 +6,19 @@ class gpioset:
     def __init__(self):
         self.led = 4
         self.button = 15
-        self.mylcd = I2C_LCD_driver.lcd()        
+        self.mylcd = I2C_LCD_driver.lcd()
+        self.buzzer = 18
+        self.scale = [261, 523]
     
     def gpioinit(self):
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(self.button, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         GPIO.setup(self.led, GPIO.OUT)
+        GPIO.setup(self.buzzer, GPIO.OUT)
+        self.p = GPIO.PWM(self.buzzer, 5)
+        self.p.start(100)
+        self.p.ChangeDutyCycle(50)
 
     def lcdplay(self, first, second):
         self.mylcd.lcd_display_string(first, 1)
@@ -24,17 +30,25 @@ class gpioset:
     def lcd_off(self):
         self.mylcd.backlight(0)
 
+    def buzzer_off(self):
+        self.p.stop()
+        GPIO.cleanup()
+
+
     def run(self):
         self.gpioinit()
         def button_callback(channel):
             GPIO.output(self.led, 0)
         while 1:
             GPIO.output(self.led, 1)
+            self.p.ChangeFrequency(self.scale[0])
             time.sleep(0.5)
             GPIO.output(self.led, 0)
+            self.p.ChangeFrequency(self.scale[1])
             time.sleep(0.5)
             if GPIO.input(self.button) == 1:
                 GPIO.output(self.led, 0)
+                self.buzzer_off()
                 break
 
 if __name__ == "__main__":
